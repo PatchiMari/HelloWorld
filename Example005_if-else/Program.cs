be6bb69bf754f51937119fe57f7571ae12ed1fd5
Maria Vorontsova <miss.konyaeva@yandex.ru>
2023-11-30 16:51:51 +0300

    add example005_if-else. не работает username.tolower() для нижнего регистра

---
+++ Example005_if-else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Мария")
+{
+        Console.WriteLine("Ура, это же Мария!");
+}
+else
+{
+        Console.Write("Привет, ");
+        Console.WriteLine(username);
+}
