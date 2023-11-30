a630efa4a2dfcd0f8a23108152df4cea5dcbd4d1
Maria Vorontsova <miss.konyaeva@yandex.ru>
2023-11-30 15:55:37 +0300

    add examples002 hello user

---
+++ Examples002_username/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
+Console.Write("введите своё имя: ");
+string username = Console.ReadLine();
+Console.Write($"Привет, {username}");
